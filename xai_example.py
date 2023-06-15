f6a8fc042bcc951895f91715a00b4401169dbe4c
J.P Lee <koreanfeel@gmail.com>
2023-06-16 00:22:58 +0900

    Analytic Statement 분석 결과 추가

--- xai_example.py
+++ xai_example.py
@@ -137,8 +137,6 @@ def main(cfg: DictConfig) -> None:
     prompt = f"""다음 정관 문서를 읽고 질문에 답하세요.
 
 정관 문서: 제 5 장 이사·이사회 제29조(이사의 수) ① 본 회사의 이사는 3인 이상 9인 이하로 한다. ② 이사는 사내이사, 사외이사와 그 밖에 상시적인 업무에 종사하지 아니하는 이사로 구분하고, 사외이사는 3인 이상으로 하되, 이사 총수의 과반수로 한다. <개정 1997.5.31, 1998.5.30, 2000.5.27, 2004.6.4, 2012.6.5, 2013.6.27, 2017.3.24> 제30조(이사의 선임) ① 이사는 주주총회에서 선임한다. <개정 2000.5.27> ② 2인 이상의 이사를 선임하는 경우에도 상법 제382조의 2에서 규정하는 집중투표제를 적용하지 아니한다. [신설 1999.5.29］ 제30조의2(임원 후보의 추천) ① 임원후보추천위원회는 금융회사의 지배구조에 관한 법률, 상법 등 관련 법규에서 정한 자격을 갖춘 자 중에서 임원(대표이사, 사외이사, 감사위원에 한함) 후보를 추천한다. [신설 2012.6.5, 개정2017.3.24］ ② 임원 후보의 추천 및 자격심사에 관한 세부적인 사항은 임원후보추천위원회에서 정한다. [신설 2012.6.5, 개정2017.3.24] 제31조(이사의 임기) ① 이사의 임기는 2년 이내로 하되, 연임할 수 있다. 다만, 사외이사는 회사에서 사외이사로 6년 이상 재직할 수 없고, 회사의 계열회사에서 사외이사로 재직한 기간을 합하여 9년 이상 재직할 수 없다. <개정 2005.3.10, 2010.5.28, 2012.6.5, 2015.3.27, 개정2017.3.24> ② 제1항의 임기가 최종 결산기 종료 후 해당 결산기에 관한 정기주주총회 전에 만료될 경우에는 그 정기주주총회의 종결시까지 그 임기가 연장된다. <개정 1996.5.25, 2000.5.27, 2005.5.27, 2013.6.27, 2014.12.17> 제32조(이사의 보선) ① 이사 중 결원이 생긴 때에는 주주총회에서 이를 선임한다. 그러나 이 정관 제29조에서 정하는 원수를 결하지 아니하고 업무수행상 지장이 없는 경우에는 그러하지 아니한다. <개정 2000.5.27, 2012.6.5> ② 보궐선임된 이사의 임기는 제31조에 따른다. <개정 2000.5.27>
-질문: 이사는 어디에서 선임하는가?
-보기: (A) 주주총회 (B) 이사회 (C) N/D
     """
 
     #
@@ -178,6 +176,12 @@ def main(cfg: DictConfig) -> None:
 
             return gr.Radio.update(choices=related_questions)
 
+        def click_button2(value):
+            print('click_button2:', value)
+            text = dummy_output2
+            time.sleep(random.randint(1, 3))
+            return gr.Label.update(text)
+
         def click_radio(value):
             print(value)
             time.sleep(random.randint(1, 3))
@@ -208,23 +212,19 @@ def main(cfg: DictConfig) -> None:
         input_file.upload(fn=upload_input_file)
         file_btn = gr.Button("업로드")
 
-        with gr.Tab('정관 샘플 1'):
-            text1 = gr.Text(prompt, label='정관 문단 입력', max_lines=10)
-            btn1 = gr.Button("분석")
-
-        with gr.Tab('정관 샘플 2'):
-            text2 = gr.Text('', label='정관 문단 입력', max_lines=10)
-            # text2.value = 'test'
-            btn2 = gr.Button("분석")
+        text1 = gr.Text(prompt, label='정관 내용', max_lines=10)
+        btn1 = gr.Button("분석")
 
         gr.Markdown("""## 위 정관 내용에서 분석되어야 할 항목입니다.""")
 
         question_label = gr.Radio(label='변호사 체크 리스트', info='정관 문단에서 예상되는 질문을 분석하여 출력합니다.', choices=[], interactive=True)
-        output_label = gr.Label(label='분석결과')
+        output_label = gr.Label(label='분석된 답변')
         question_label.input(fn=click_radio, inputs=question_label, outputs=output_label, queue=False)
+        btn2 = gr.Button("Analytic Statement 분석")
+        output_label2 = gr.Label(label='Analytic Statement 분석 결과')
+        btn2.click(fn=click_button2, inputs=output_label, outputs=output_label2, queue=False)
 
         btn1.click(fn=click_button, inputs=text1, outputs=question_label, queue=False)
-        btn2.click(fn=click_button, inputs=text2, outputs=question_label, queue=False)
 
         file_btn.click(fn=upload_input_file, inputs=input_file, outputs=text1, queue=False)
 
